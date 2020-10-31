d5c5c7a85304bb669d0192c1c6b8147d196fb33b
vivi <me@vivi.fyi>
2020-10-31 21:27:31 +0100

    Improve id generation and something redis

--- src/nanoid.ts
+++
@@ -1,3 +0,0 @@
-import { customAlphabet } from "nanoid/async"
-
-export const nanoid = customAlphabet("123456789abcdefghjkmnpqrstuvwxyz", 8)
--- src/routes/create.ts
+++ src/routes/create.ts
@@ -2,8 +2,8 @@ import Router from "@koa/router"
 import * as yup from "yup"
 import { MAX_TTL, PUBLIC_URL } from "../config"
 import { db } from "../db"
+import { getUniqueId } from "../id"
 import { validate } from "../middleware/validate"
-import { nanoid } from "../nanoid"
 import { allowedOrigin } from "../validation/allowedOrigin"
 
 export const router = new Router()
@@ -16,15 +16,12 @@ const schema = yup.object().shape({
 router.post("/create", validate(schema), async (context) => {
   const { url, ttl } = context.request.body
 
-  const id = await nanoid()
-
-  const expiresAt = new Date(Date.now() + ttl * 1000)
-  await db.set(id, url)
-  await db.pexpireat(id, expiresAt.getTime())
+  const id = await getUniqueId()
+  await db.set(id, url, "PX", ttl * 1000)
 
   context.body = {
     id,
     url: `${PUBLIC_URL ?? context.origin}/go/${id}`,
-    expires: expiresAt,
+    expires: new Date(Date.now() + ttl * 1000),
   }
 })
