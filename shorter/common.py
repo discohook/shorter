18a72495127ed0f564c273879497ff87b3f2c7a4
vivi <me@vivi.fyi>
2020-11-18 14:13:27 +0100

    Fix shortname duplication checks

--- shorter/common.py
+++ shorter/common.py
@@ -9,8 +9,9 @@ ALPHABET = string.digits + string.ascii_lowercase
 async def generate_shortname(length: int):
     for _ in range(16):
         possible_shortname = "".join(secrets.choice(ALPHABET) for _ in range(length))
+        key = f"{current_app.import_name}-shorten-{possible_shortname}"
 
-        if not await current_app.redis.exists(possible_shortname):
+        if not await current_app.redis.exists(key):
             return possible_shortname
 
     return generate_shortname(length + 1)
